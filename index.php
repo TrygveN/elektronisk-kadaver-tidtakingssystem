0fc2aa5f05ebd3af54c315828e51e70ce9aa1c04
Trygve <trygve@trygve.net>
2024-10-08 21:41:05 +0200

    Auto oppdatering hvert 5. sekund

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html lang="no">
 <head>
+    <meta charset="UTF-8" />
+    <title>EKT</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <meta name="description" content="Elektronisk Kadaver Tidtakningssystem" />
     <link rel="stylesheet" href="https://matcha.mizu.sh/matcha.css">
 </head>
 <body>
@@ -14,4 +18,16 @@ include("get_table.php")
 </tbody>
 </table>
 </body>
+<script>
+    function update() {
+        const table = document.querySelector("table");
+        const myRequest = new Request(`get_table.php`);
+        fetch(myRequest)
+        .then((response) => response.text())
+        .then((text) => {
+        table.innerHTML = text;
+        });
+    }
+    setInterval(update, 5*1000)
+</script>
 </html>
