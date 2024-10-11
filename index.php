a51be0c2bf1983c13cc37b42fd80cc915ce8b6f3
Trygve <trygve@trygve.net>
2024-10-11 17:41:43 +0200

    pynt

--- index.php
+++ index.php
@@ -6,9 +6,19 @@
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <meta name="description" content="Elektronisk Kadaver Tidtakningssystem" />
     <link rel="stylesheet" href="https://matcha.mizu.sh/matcha.css">
+    <style>
+        @media (prefers-color-scheme: dark) {
+            .invert {
+                filter: invert(1);
+            }
+        }
+    </style>
 </head>
 <body>
+<div class="flex align-center">
+<figure><img src="img/kadaver.png" alt="" width="100px" class="invert"></figure>
 <h1>Løpende resultater kadaverløpet 2024</h1>
+</div>
 <h2>Vi tar forbehold om feil. Dette er ikke offisielle resultater</h2>
 <table>
 <tbody>
@@ -16,7 +26,11 @@
 include("get_table.php")
 ?>
 </tbody>
+<footer>
 </table>
+<h3>Laget av Trygve. <a href="https://git.willy.club/Trygve/elektronisk-kadaver-tidtakingssystem">Kildekode</a></h3>
+<figure><img src="img/NMBUI.webp" alt="" width="200px"></figure>
+</footer>
 </body>
 <script>
     function update() {
