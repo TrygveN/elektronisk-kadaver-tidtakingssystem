91451b382ff0ba64b0ad91b26c9d02ea563a5a0b
Trygve <trygve@trygve.net>
2025-10-27 11:36:58 +0100

    Lagde innloggingsise

---
+++ is_authorized.php
@@ -0,0 +1,16 @@
+<?php
+$hash = file_get_contents("hash.txt");
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method == "POST") {
+    $password = $_POST['password'];
+    if (!password_verify($password, $hash)) {
+        http_response_code(response_code: 401);
+    }
+    else {
+        http_response_code(response_code: 200);
+    }
+}
+else {
+        http_response_code(response_code: 405);
+    }
+
